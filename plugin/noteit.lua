92a8e2680854c1a735b1c04423e52c50bc9e324c
Fredrik Skarhed <fredrik.skarhed@westermo.com>
2026-08-24 14:33:30 +0200

    Removed hardcoded keymaps, it should be up to the user to set keymaps to minimize conflicts in mapping
    Signed-off-by: Fredrik Skarhed <fredrik.skarhed@westermo.com>

--- plugin/noteit.lua
+++ plugin/noteit.lua
@@ -9,8 +9,3 @@ vim.api.nvim_create_user_command("NoteAdd", noteit.add_note, {})
 vim.api.nvim_create_user_command("NoteRemove", noteit.remove_note, {})
 vim.api.nvim_create_user_command("NoteShow", noteit.show_note, {})
 vim.api.nvim_create_user_command("NoteList", noteit.show_notes, {})
-
-vim.keymap.set("n", "<leader>na", "<cmd>NoteAdd<CR>", { desc = "Add note", silent = true })
-vim.keymap.set("n", "<leader>nl", "<cmd>NoteList<CR>", { desc = "List notes", silent = true })
-vim.keymap.set("n", "<leader>ns", "<cmd>NoteShow<CR>", { desc = "Show note", silent = true })
-vim.keymap.set("n", "<leader>nr", "<cmd>NoteRemove<CR>", { desc = "Remove note", silent = true })
